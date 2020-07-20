5b1af5d05467a553aa72e6b77dd7f0f6dd8ca2c5
Kevin Toh <kevintoh0305@gmail.com>
2020-07-20 23:02:33 +0800

    Site without countdown

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var events = [
+    {
+        "event_name" : "BMT Passing Out Parade",
+        "date" : "11/12/2020"
+    },
+    {
+        "event_name" : "Operational Ready Date",
+        "date" : "11/8/2022"
+    }
+]
