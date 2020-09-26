1c73371aeab146a7216edfc6a3b800daf69261f7
Kevin Toh <kevintoh0305@gmail.com>
2020-09-26 22:03:25 +0800

    Week 8 shit

--- js/app.js
+++ js/app.js
@@ -59,11 +59,49 @@ var milestones = [
     new Milestone("1 Month In NS 🌞", new Date("Sep 11, 2020 00:00:00"), "timer-0003"),
     new Milestone("3rd Bookout 😲", new Date("Sep 12, 2020 17:00:00"), "timer-0004"),
     new Milestone("4th Bookout 🙃", new Date("Sep 17, 2020 16:00:00"), "timer-0005"),
-    new Milestone("5th Bookout 🥴", new Date("Sep 25, 2020 18:00:00"), "timer-0006"),
-    new Milestone("Last Day in Tekong ✨", new Date("Dec 4, 2020 00:00:00"), "timer-0007"),
-    new Milestone("BMT Passing Out Parade 💪", new Date("Dec 5, 2020 12:00:00"), "timer-0008"),
-    new Milestone("Operational Ready Date 🎉", new Date("Aug 10, 2022 18:00:00"), "timer-0009"),
+    new Milestone("5th Bookout 🥴", new Date("Sep 25, 2020 19:00:00"), "timer-0006"),
+    new Milestone("6th Bookout 😢", new Date("Oct 2, 2020 19:00:00"), "timer-0007"),
+    new Milestone("Last Day in Tekong ✨", new Date("Dec 4, 2020 00:00:00"), "timer-0008"),
+    new Milestone("BMT Passing Out Parade 💪", new Date("Dec 5, 2020 12:00:00"), "timer-0009"),
+    new Milestone("Operational Ready Date 🎉", new Date("Aug 10, 2022 18:00:00"), "timer-0010"),
 ]
+
+/*
+var ring = document.getElementsByTagName('path')[0];
+// var range = document.getElementsByTagName('input')[0];
+var text = document.getElementsByTagName('text')[0];
+var toRadians = Math.PI / 180;
+var r = 100;
+
+function draw() {
+  // Update the wheel giving to it a value in degrees,
+  // getted from the percentage of the input value
+  // a.k.a. (value * 360) / 100
+  //   var degrees = range.value * 3.5999;
+  var degrees = 30 * 3.5999; //Hardcoded
+  // Convert the degrees value to radians
+  var rad = degrees * toRadians;
+  // Determine X and cut to 2 decimals
+  var x = (Math.sin(rad) * r).toFixed(2);
+  // Determine Y and cut to 2 decimals
+  var y = -(Math.cos(rad) * r).toFixed(2);
+  // The another half ring. Same as (deg > 180) ? 1 : 0
+  var lenghty = Number(degrees > 180);
+  // Moveto + Arcto
+  var descriptions = ['M', 0, 0, 'v', -r, 'A', r, r, 1, lenghty, 1, x, y, 'z'];
+  // Apply changes to the path
+  ring.setAttribute('d', descriptions.join(' '));
+  // Update the numeric display
+  text.textContent = range.value;
+}
+
+// Translate the center axis to a half of total size
+ring.setAttribute('transform', 'translate(' + r + ', ' + r + ')');
+// Bind the change event to the input
+range.addEventListener('input', draw);
+// Force to init the first time;
+
+*/
 
 window.onload = ()=>{
     let content = document.getElementById("content");
